ba0745037ffbd3ef94f872fc7dacf2ed4b2d6a6b
satishavhad <satishavhad@gmail.com>
2017-10-15 21:22:12 -0400

    first draft

--- main.py
+++ main.py
@@ -33,7 +33,13 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
     
-    return None, None, None, None, None
+    tf.save_model.loader.load(sess, [vgg_tag], vgg_tag)
+    graph = tf.get_default_graph()
+    w1 = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    
+    return w1, keep, None, None, None
+
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,6 +53,8 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
+    conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    output = tf.layers.conv2d_transpose(conv_1x1, num_classes, 4, 2, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     return None
 tests.test_layers(layers)
 
@@ -61,6 +69,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
+    logits = tf.reshape(input, (-1, num_classes))
     return None, None, None
 tests.test_optimize(optimize)
 
@@ -81,6 +90,10 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
+    for epochs in epochs:
+        for image, label in get_batches_fn(batch_size):
+            #Training
+            pass
     pass
 tests.test_train_nn(train_nn)
 
@@ -109,8 +122,11 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
-
+        input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
+        layer_output = layers(layer3_out, layer4_out, layer7_out, num_classes)
+        
         # TODO: Train NN using the train_nn function
+        # SATISH - Call optimizer function from here
 
         # TODO: Save inference data using helper.save_inference_samples
         #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
